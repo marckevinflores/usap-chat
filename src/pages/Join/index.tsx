0ecbb9932d22c32de2f39e92faf77a5bb5ad6776
Marc Kevin Flores <marckevinflores@gmail.com>
2024-03-05 22:10:45 +0800

    fix: anchor url

--- src/pages/Join/index.tsx
+++ src/pages/Join/index.tsx
@@ -22,11 +22,11 @@ export const Join = ({ username, setUsername, room, setRoom, socket }: JoinProps
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-            <a href="#"
+            <div
                 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <Logo/>
                 Usap Chat
-            </a>
+            </div>
             <div
                 className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
